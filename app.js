7f0320163e915bcefb6069abf38125bbefefd266
alvaromenegon <alvaroeduardo@alunos.utfpr.edu.br>
2022-05-05 14:20:32 -0300

    2nd commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const db_mongoose = require('./config/db_mongoose');
 const routes = require('./routers/route');
 const mongoose = require('mongoose');
 const express = require('express');
-const unirest = require('unirest');
+//const unirest = require('unirest');
 const app = express();
 
 app.use(express.json());
@@ -26,8 +26,6 @@ app.use(
 var porta = process.env.PORT || 8081;
 
 app.listen(porta);
-
-
 //var receitas = unirest.get('http://localhost:8081/api/receitas');
 //console.log(receitas);
 
